6e9ed3e76e53cc5085517aa320a2483d6cbc339e
shine-sh <lafoiaudestin@gmail.com>
2021-05-13 18:27:43 +0300

    Update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+var dataSourceUrl = "https://api.le-systeme-solaire.net/rest/bodies";
+var fileDataSourceUrl = "bodies.json";
+
 async function getData(url) {
     var response = await fetch(url);
     var data = await response.json();
@@ -5,43 +8,45 @@ async function getData(url) {
 }
 
 getData(dataSourceUrl).then(function (data) {
-    console.log(data);
-});
+    var bodies = data.bodies;
+    var bodiesWithMoons = bodies.filter (function(body) {
+        return body.moons
+    });
 
-console.log(Chart);
+    var BodiesSortedByMoonCount = bodiesWithMoons.sort(function(a, b) {
+        return a.moons.lenght > b.moons.length ? 1 : -1
+    });
 
-var ctx = document.getElementById('chart-1').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
+    var bodyNames = bodiesWithMoons.map(function (body) {
+        return body.englishName;
+    })
+    var moonCounts = bodiesWithMoons.map(function (body) {
+        return body.moons.length;
+    });
+
+    console.log(Chart);
+    var ctx = document.getElementById('chart-1').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: bodyNames,
+            datasets: [{
+                label: '# of Moons',
+                data: moonCounts,
+                borderWidth: 3,
+                bordercolor: '#ff00ff'
+                borderDash: [10],
+
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    min: 0;
+                    max: 100,
+                },
+            },
+        },
+    });       
 });
